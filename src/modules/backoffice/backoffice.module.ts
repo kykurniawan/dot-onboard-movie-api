b3c778568935c123055225e45c46e06dac9945f1
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-08-28 22:41:47 +0800

    update multer configuration

--- src/modules/backoffice/backoffice.module.ts
+++ src/modules/backoffice/backoffice.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, BadRequestException } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
@@ -8,13 +8,29 @@ import { BackofficeController } from './backoffice.controller';
 @Module({
   imports: [
     MulterModule.register({
-      dest: './uploads',
       storage: diskStorage({
         destination: './uploads/movie_posters',
         filename(req, file, callback) {
           callback(null, Date.now() + extname(file.originalname));
         },
       }),
+      fileFilter(req, file, callback) {
+        if (file.mimetype !== 'image/jpeg') {
+          return callback(
+            new BadRequestException(
+              'Only file with image/jpg mimetype are allowed',
+            ),
+            false,
+          );
+        }
+        if (file.size > 2000000) {
+          return callback(
+            new BadRequestException('Maximum file size is 2MB'),
+            false,
+          );
+        }
+        return callback(null, true);
+      },
     }),
     MovieModule,
   ],
